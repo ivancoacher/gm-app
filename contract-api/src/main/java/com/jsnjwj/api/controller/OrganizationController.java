25bcbd1f8d9e31b90c2b0f227f494b738bbeade4
ivan <769964834@qq.com>
2023-07-07 15:07:17 +0800

    remove userService

--- contract-api/src/main/java/com/jsnjwj/api/controller/OrganizationController.java
+++ contract-api/src/main/java/com/jsnjwj/api/controller/OrganizationController.java
@@ -1,12 +1,9 @@
 package com.jsnjwj.api.controller;
 
-import com.jsnjwj.compare.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.annotation.Resource;
 
 @Slf4j
 @Controller
@@ -14,13 +11,10 @@ import javax.annotation.Resource;
 
 public class OrganizationController {
 
-	@Resource
-	private UserService userService;
-
 	@ResponseBody
 	@RequestMapping("/")
 	public Integer index() {
-		return userService.save();
+		return 2222;
 	}
 
 }
